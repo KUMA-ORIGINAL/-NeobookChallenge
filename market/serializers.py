fe2b90a9421728362b8c51c87fa2dc54e07b18ce
KUMA-ORIGINAL <108028683+KUMA-ORIGINAL@users.noreply.github.com>
2023-12-20 16:02:18 +0000

    Update serializers.py and test

--- market/serializers.py
+++ market/serializers.py
@@ -38,12 +38,12 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = '__all__'
 
-    def create(self, validated_data):
-        products_data = validated_data.pop('products')
-        order = Order.objects.create(**validated_data)
-        for product_data in products_data:
-            OrderItem.objects.create(order=order, **product_data)
-        return {
-            'id': order.id,
-            'created': order.created
-        }
+    # def create(self, validated_data):
+    #     products_data = validated_data.pop('products')
+    #     order = Order.objects.create(**validated_data)
+    #     for product_data in products_data:
+    #         OrderItem.objects.create(order=order, **product_data)
+    #     return {
+    #         'id': order.id,
+    #         'created': order.created
+    #     }
